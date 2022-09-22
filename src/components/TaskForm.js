572d927637a0d998d87e21c9bb45112280f8bab6
Adrian Hernandez <danix08@live.com>
2022-09-22 01:25:11 -0300

    agregando useDispatch y objetos

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,5 +1,44 @@
-function TaskForm() {
-    return <div>TaskForm</div>;
-}
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTask } from "../features/counter/taskSlice";
+
+const TaskForm = () => {
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addTask(task));
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        placeholder="title"
+        onChange={handleChange}
+      />
+
+      <textarea
+        name="description"
+        placeholder="description"
+        onChange={handleChange}
+      ></textarea>
+
+      <button>Save</button>
+    </form>
+  );
+};
 
 export default TaskForm;
